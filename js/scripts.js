9e0103ac7b3fdc1c20a0cbc524b35cd2cefa1943
marblo <marcinblochowiak@wp.pl>
2017-04-28 20:37:15 +0200

    test

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var famaleNames = ['Magda', 'Ania', 'Gosia', 'Dominika'];
+var maleNames = ['Tomasz', 'Maciej', 'Bernard', 'Marcin'];
+var allNames = famaleNames.concat(maleNames);
+var newName = 'Marian';
+
+if (allNames.indexOf(newName) === -1 ) {
+	var x = allNames.push(newName);
+} else {
+	alert('Name exist');
+}
+
+console.log(allNames);
